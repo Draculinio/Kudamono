494e42ae6a24d53471d66fad6e78fcd9417a1807
Pablo Soifer <stackpointerex@gmail.com>
2018-06-30 16:38:13 -0300

    Trabajando con subprocesos en autom.py

--- autom.py
+++ autom.py
@@ -2,27 +2,37 @@
 import os
 import json
 import requests
+import subprocess
 
-os.system("chromedriver.exe --port=9000")
+import threading
 
-#session creation
-my_json =  {
-   "desiredCapabilities": {
-      "browserName": "chrome",
-      "chromeOptions": {
-         "binary": "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-      },
-      "platform": "ANY"
-   }
-}
+#os.system("chromedriver.exe --port=9000")
+def server():
+    subprocess.Popen("chromedriver.exe --verbose --port=9000")
 
-url = "http://127.0.0.1:9000/session"
+def flow():
+    #session creation
+    my_json =  {
+    "desiredCapabilities": {
+       "browserName": "chrome",
+       "chromeOptions": {
+          "binary": "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+       },
+       "platform": "ANY"
+        }
+    }
 
-response = requests.request("POST",url,data=json.dumps(my_json).encode('utf8'))
+    url = "http://127.0.0.1:9000/session"
 
-print(response.text)
-print(response.headers) 
-session = json.loads(response.text)['sessionId']
-#Session destruction
-response = requests.request("DELETE",url+"/"+session)
-print(response.text)
+    response = requests.request("POST",url,data=json.dumps(my_json).encode('utf8'))
+
+    print(response.text)
+    print(response.headers)
+    session = json.loads(response.text)['sessionId']
+    #Session destruction
+    response = requests.request("DELETE",url+"/"+session)
+    print(response.text)
+
+
+server()
+flow()
--- webDriver.py
+++ webDriver.py
@@ -5,20 +5,29 @@ class webDriver():
     def __init__(self,browser):
         self.browser = browser
         self.url = 'http://127.0.0.1:9000/'
+
     def start_browser(self):
-        #Open server
-        os.system("chromedriver.exe --port=9000")
-        browser_configuration =  { #TODO: Start working in a json parser
-           "desiredCapabilities": {
-              "browserName": "chrome",
-              "chromeOptions": {
-                 "binary": "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-              },
-              "platform": "ANY"
-           }
-        }
-        response = requests.request("POST",self.url,data=json.dumps(my_json).encode('utf8'))
+        self.open_server("chrome")
+        self.create_session()
         
 
     def end_browser(self,session_id):
         response = requests.request("DELETE",self.url+"/session"+session_id)
+
+    #MAYBE METHODS BELOW SHOULD GO IN A SEPARATE CLASS?
+
+    def open_server(self,server):
+        if str.upper(server) == "CHROME":
+            os.system("chromedriver.exe --port=9000")
+
+    def create_session(self):
+        browser_configuration = {  # TODO: Start working in a json parser
+            "desiredCapabilities": {
+                "browserName": "chrome",
+                "chromeOptions": {
+                    "binary": "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+                },
+                "platform": "ANY"
+            }
+        }
+        response = requests.request("POST", self.url, data=json.dumps(browser_configuration).encode('utf8'))
