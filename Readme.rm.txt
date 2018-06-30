778fc8d13ced25794e29f7ddfba464f8b4591f8e
Pablo Soifer <stackpointerex@gmail.com>
2018-06-30 14:06:48 -0300

    Starting to work on webDriver class

--- Readme.rm.txt
+++ Readme.rm.txt
@@ -1,3 +1,11 @@
+06/30/2018
+
+Starting to work on my webdriver class, my biggest challenge now is to be able to run the driver on a thread and continue the operations
+in another.
+
+Another thing I should start doing is to make this a real project with some management.
+
+06/29/2018
 Welcome to my automation project.
 
 For now we have just some experimentation buuuut, it will grow.
--- webDriver.py
+++ webDriver.py
@@ -1,6 +1,24 @@
+import requests
+import json
+import os
 class webDriver():
     def __init__(self,browser):
         self.browser = browser
-
-    def start_browser(self)
+        self.url = 'http://127.0.0.1:9000/'
+    def start_browser(self):
+        #Open server
+        os.system("chromedriver.exe --port=9000")
+        browser_configuration =  { #TODO: Start working in a json parser
+           "desiredCapabilities": {
+              "browserName": "chrome",
+              "chromeOptions": {
+                 "binary": "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+              },
+              "platform": "ANY"
+           }
+        }
+        response = requests.request("POST",self.url,data=json.dumps(my_json).encode('utf8'))
         
+
+    def end_browser(self,session_id):
+        response = requests.request("DELETE",self.url+"/session"+session_id)
