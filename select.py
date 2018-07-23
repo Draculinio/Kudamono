464cff0c46f7241ac42ca7078c76f819a48b1ea5
Pablo S Soifer <ssoifer@belatrixsf.com>
2018-07-23 13:12:41 -0300

    Select by index added

--- select.py
+++ select.py
@@ -5,7 +5,8 @@ class Select:
     def __init__(self,element,driver):
         self.element = element
         self.driver = driver
-    #SELECTS... this is gonna be hard...
+    #SELECTS... this is gonna be hard... 
+    # 07/23/2017 Well, it wasn't.
     def select_by_text(self,text):
         """
         Selects item by text
@@ -19,5 +20,16 @@ class Select:
         self.driver.click(self.element)
         self.driver.write(self.element,my_key.get_key('HOME'))
         for i in range(0,number):
+            self.driver.write(self.element,my_key.get_key('KEYDOWN'))
+        self.driver.write(self.element,my_key.get_key('RETURN'))
+
+    def select_by_index(self,index):
+        """
+        Selects item by index
+        """
+        my_key = Keys()
+        self.driver.click(self.element)
+        self.driver.write(self.element,my_key.get_key('HOME'))
+        for i in range(0,index):
             self.driver.write(self.element,my_key.get_key('KEYDOWN'))
         self.driver.write(self.element,my_key.get_key('RETURN'))
